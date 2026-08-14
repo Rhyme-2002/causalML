f0832d187f10c3c69a3a69fcabd3e94161c9e225
Rhyme-2002 <asrhyme@isrt.ac.bd>
2026-08-15 04:31:19 +0600

    Update example to exclude intercept in model matrix

--- R/predict.causalMARS.R
+++ R/predict.causalMARS.R
@@ -50,7 +50,7 @@
 #' \dontrun{
 #' x1 <- rnorm(100)
 #' x2 <- rnorm(100)
-#' X <- model.matrix(~ x1 + x2)
+#' X <- model.matrix(~ x1 + x2)[, -1]
 #' Y <- rnorm(100)
 #' treatment <- rbinom(100, 1, 0.5)
 #'
